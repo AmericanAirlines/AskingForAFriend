34df5d47932219601e6191c2272caf1adcbfb413
Demondre Livingston <livingstondemondre7@gmail.com>
2021-05-14 13:49:37 -0400

    More files

---
+++ src/slack/common/ViewOutputUtils.ts
@@ -0,0 +1,30 @@
+import { ViewOutput } from '@slack/bolt';
+
+interface BlockInputValue {
+  value?: string;
+  selected_option?: { value: string };
+  selected_users?: string[];
+  selected_channel: string;
+}
+
+export class ViewOutputUtils {
+  readonly view: ViewOutput;
+
+  constructor(output: ViewOutput) {
+    this.view = output;
+  }
+
+  /**
+   * Returns the first input value with the given actionId
+   * @param actionId The action_id for the input element, set in the view blocks
+   */
+  getInputValue(actionId: string): BlockInputValue | undefined {
+    for (const blockValue of Object.values(this.view.state.values)) {
+      if (blockValue[actionId]) {
+        return blockValue[actionId];
+      }
+    }
+
+    return undefined;
+  }
+}
