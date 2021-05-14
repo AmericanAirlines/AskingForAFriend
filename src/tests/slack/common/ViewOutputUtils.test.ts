25aec4924d255a7928c510df21a4a4edfaa04d5f
Spencer Kaiser <spencer.kaiser@aa.com>
2021-05-14 15:30:56 -0500

    Fixing a prettier issue

--- src/tests/slack/common/ViewOutputUtils.test.ts
+++ src/tests/slack/common/ViewOutputUtils.test.ts
@@ -6,7 +6,7 @@ describe('view output utils', () => {
   it('gets the correct value for a specified field', () => {
     const mockActionId = 'something';
     const mockValue = 'a selected value';
-    const mockViewOutput = ({
+    const mockViewOutput = {
       state: {
         values: [
           {
@@ -15,22 +15,22 @@ describe('view output utils', () => {
           },
         ],
       },
-    } as unknown) as ViewOutput;
+    } as unknown as ViewOutput;
     const viewOutputUtils = new ViewOutputUtils(mockViewOutput);
     expect(viewOutputUtils.getInputValue(mockActionId)).toBe(mockValue);
   });
   it('returns undefined when the field cannot be found', () => {
-    const mockViewOutput = ({
+    const mockViewOutput = {
       state: {
         values: [{}],
       },
-    } as unknown) as ViewOutput;
+    } as unknown as ViewOutput;
     const viewOutputUtils = new ViewOutputUtils(mockViewOutput);
     expect(viewOutputUtils.getInputValue('someUnknownActionId')).toBeUndefined();
   });
 
   it('returns undefined if a block does not have an action ID', () => {
-    const mockViewOutput = ({
+    const mockViewOutput = {
       state: {
         values: [
           {
@@ -38,7 +38,7 @@ describe('view output utils', () => {
           },
         ],
       },
-    } as unknown) as ViewOutput;
+    } as unknown as ViewOutput;
     const viewOutputUtils = new ViewOutputUtils(mockViewOutput);
     expect(viewOutputUtils.getInputValue('someUnknownActionId')).toBeUndefined();
   });
